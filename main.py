fc31bb2bc29462e5e15887e676af89da98324661
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2025-07-20 20:33:56 -0700

    CH1: LLMs L3: Gemini

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from boot-dev-ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
-
-if __name__ == "__main__":
-    main()
+load_dotenv()
+model = "gemini-2.0-flash-001"
+content_string = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(model=model, contents=content_string)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
