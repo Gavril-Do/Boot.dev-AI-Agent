db568973faff7b6737148a6f3f5750f2d0e7c2b8
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2025-07-20 21:31:17 -0700

    CH1: LLMs L4: Input

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
+if len(sys.argv) == 1:
+    print("Error: No prompt")
+    sys.exit(1)
 load_dotenv()
 model = "gemini-2.0-flash-001"
-content_string = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+content_string = sys.argv[1] + "Use no more than 250 characters."
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(model=model, contents=content_string)
+print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
