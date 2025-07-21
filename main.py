a26e0644e521d9d6a283056245a7a71e64e4d772
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2025-07-20 22:41:45 -0700

    CH1: LLMs L5: Messages

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-if len(sys.argv) == 1:
-    print("Error: No prompt")
-    sys.exit(1)
-load_dotenv()
-model = "gemini-2.0-flash-001"
-content_string = sys.argv[1] + "Use no more than 250 characters."
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model=model, contents=content_string)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+    args.append("Use less than 250 characters.")
+
+    if not args:
+        print("Error: No Prompt")
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+    print("Response:")
+    print(response.text)
